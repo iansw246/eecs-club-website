508a58dca70b9ceebe934db0a13dbb51389b10b8
Ian <ianjazz246@gmail.com>
2020-10-15 22:58:35 -0700

    Theme-adapting favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -138,7 +138,7 @@ export default function Layout({ children, stickyFooter = true}) {
 				<meta name="title" content={data.site.siteMetadata.title} />
 				<meta name="description" content={data.site.siteMetadata.description} />
 
-				<link rel="icon" type="image/svg+xml" href="/img/eecs-logo.svg" />
+				<link rel="icon" type="image/svg+xml" href="/img/favicon.svg" />
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
