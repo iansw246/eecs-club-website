040c4ebc235994777ca0b7a1aaeda4736aa1850a
Ian <ianjazz246@gmail.com>
2020-10-17 17:40:15 -0700

    Start better link widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,6 @@
 import CMS from "netlify-cms-app"
 import { URLWidgetControl, URLWidgetPreview } from "./URLWidget"
+import { LinkWidgetControl } from "./LinkWidget"
 
 CMS.registerWidget("url", URLWidgetControl, /* URLWidgetPreview */);
+CMS.registerWidget("link", LinkWidgetControl);
