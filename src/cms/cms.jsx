9b5ab7b3cef14c04e83ba53047becb37e3567993
Ian <ianjazz246@gmail.com>
2020-12-09 19:56:08 -0800

    Page Preview testing

--- src/cms/cms.jsx
+++ src/cms/cms.jsx
@@ -1,8 +1,11 @@
 import CMS from "netlify-cms-app"
-import { URLWidgetControl, URLWidgetPreview } from "./URLWidget"
-import { LinkWidgetControl } from "./LinkWidget"
-import { CroppedImagePreview } from "./CroppedImage"
+import { URLWidgetControl, URLWidgetPreview } from "./urlWidget"
+import { LinkWidgetControl } from "./linkWidget"
+import { CroppedImagePreview } from "./croppedImage"
 import Image from "netlify-cms-widget-image"
+
+import IndexPagePreview from "./preview-templates/indexPagePreview"
+import AboutPagePreview from "./preview-templates/aboutPagePreview"
 
 CMS.registerWidget("url", URLWidgetControl, /* URLWidgetPreview */);
 CMS.registerWidget("link", LinkWidgetControl);
@@ -11,3 +14,7 @@ CMS.registerWidget("link", LinkWidgetControl);
 //		image_height: "height of image. Passed to css"
 //		image_width: "width of image. Passed to css"
 CMS.registerWidget("croppedImage", Image.controlComponent, CroppedImagePreview);
+
+
+CMS.registerPreviewTemplate("index", IndexPagePreview);
+CMS.registerPreviewTemplate("about", AboutPagePreview);
